1a5ab75086ef02672c6ce755c7295eb561cfc7d1
Noah Carlos <ndcarlos415@gmail.com>
2025-04-23 17:38:06 +0700

    wrap synthetic data generation in main guard to control synthetic data

--- supply_chain_analysis/scripts/demand_forecasting.py
+++ supply_chain_analysis/scripts/demand_forecasting.py
@@ -5,73 +5,85 @@ import matplotlib.pyplot as plt
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 
-from supply_chain_analysis.scripts.synthetic_data_monthly import synthetic_df
+from supply_chain_analysis.scripts.inventory_optimization import inventory_df
+from supply_chain_analysis.scripts.inventory_optimization import inventory_summary
 
-sku0_to_forecast = synthetic_df['SKU'].iloc[0]
+synthetic_demand = pd.read_csv('supply_chain_analysis/data/processed/synthetic_monthly.csv')
 
-sku0_df = synthetic_df[synthetic_df['SKU'] == sku0_to_forecast].copy()
-
-sku0_df["moving_avg"] = sku0_df["demand"].rolling(window=3).mean()
-
-plt.figure(figsize=(10, 5))
-sns.lineplot(data=sku0_df, x="date", y="demand", label="Actual Demand")
-sns.lineplot(data=sku0_df, x="date", y="moving_avg", label="3-Month Moving Avg", linestyle="--")
-
-plt.title(f"Monthly Demand Forecast for {sku0_to_forecast}")
-plt.xlabel("Date")
-plt.ylabel("Units")
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
+head(synthetic_demand)
 
 
 
-sku0_df.set_index("date", inplace=True)
-
-model = ExponentialSmoothing(
-    sku0_df['demand'],
-    trend='add',
-    seasonal='add',
-    seasonal_periods=4
-)
-
-hw_fit = model.fit()
-
-forecast = hw_fit.forecast(6)
-
-plt.figure(figsize=(10, 5))
-sns.lineplot(data=sku0_df, x=sku0_df.index, y="demand", label="Actual Demand")
-sns.lineplot(x=forecast.index, y=forecast.values, label="Forecast", linestyle="--")
-
-plt.title("Holt-Winters Forecast with 2-Month Seasonality")
-plt.xlabel("Date")
-plt.ylabel("Demand")
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
 
 
 
-plt.figure(figsize=(12, 6))
 
-# Actual demand
-sns.lineplot(data=sku0_df, x=sku0_df.index, y="demand", label="Actual Demand")
 
-# 3-period moving average
-sns.lineplot(data=sku0_df, x=sku0_df.index, y="moving_avg", label="3-Period Moving Avg", linestyle="--")
-
-# Holt-Winters forecast
-sns.lineplot(x=forecast.index, y=forecast.values, label="Holt-Winters Forecast", linestyle="--", color="orange")
-
-plt.title("Demand Forecast Comparison")
-plt.xlabel("Date")
-plt.ylabel("Units")
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.legend()
-plt.tight_layout()
-plt.show()
+# sku0_to_forecast = synthetic_df['SKU'].iloc[0]
+#
+# sku0_df = synthetic_df[synthetic_df['SKU'] == sku0_to_forecast].copy()
+#
+# sku0_df["moving_avg"] = sku0_df["demand"].rolling(window=3).mean()
+#
+# plt.figure(figsize=(10, 5))
+# sns.lineplot(data=sku0_df, x="date", y="demand", label="Actual Demand")
+# sns.lineplot(data=sku0_df, x="date", y="moving_avg", label="3-Month Moving Avg", linestyle="--")
+#
+# plt.title(f"Monthly Demand Forecast for {sku0_to_forecast}")
+# plt.xlabel("Date")
+# plt.ylabel("Units")
+# plt.xticks(rotation=45)
+# plt.grid(True)
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
+#
+#
+#
+# sku0_df.set_index("date", inplace=True)
+#
+# model = ExponentialSmoothing(
+#     sku0_df['demand'],
+#     trend='add',
+#     seasonal='add',
+#     seasonal_periods=4
+# )
+#
+# hw_fit = model.fit()
+#
+# forecast = hw_fit.forecast(6)
+#
+# plt.figure(figsize=(10, 5))
+# sns.lineplot(data=sku0_df, x=sku0_df.index, y="demand", label="Actual Demand")
+# sns.lineplot(x=forecast.index, y=forecast.values, label="Forecast", linestyle="--")
+#
+# plt.title("Holt-Winters Forecast with 2-Month Seasonality")
+# plt.xlabel("Date")
+# plt.ylabel("Demand")
+# plt.xticks(rotation=45)
+# plt.grid(True)
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
+#
+#
+#
+# plt.figure(figsize=(12, 6))
+#
+# # Actual demand
+# sns.lineplot(data=sku0_df, x=sku0_df.index, y="demand", label="Actual Demand")
+#
+# # 3-period moving average
+# sns.lineplot(data=sku0_df, x=sku0_df.index, y="moving_avg", label="3-Period Moving Avg", linestyle="--")
+#
+# # Holt-Winters forecast
+# sns.lineplot(x=forecast.index, y=forecast.values, label="Holt-Winters Forecast", linestyle="--", color="orange")
+#
+# plt.title("Demand Forecast Comparison")
+# plt.xlabel("Date")
+# plt.ylabel("Units")
+# plt.xticks(rotation=45)
+# plt.grid(True)
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
--- supply_chain_analysis/scripts/synthetic_data_monthly.py
+++ supply_chain_analysis/scripts/synthetic_data_monthly.py
@@ -1,28 +1,54 @@
 import pandas as pd
 import numpy as np
+import os
+
+from pandas.core.interchange.dataframe_protocol import DataFrame
 
 from supply_chain_analysis.scripts.inventory_optimization import inventory_df
 
-# Define date range (12 months)
-months = pd.date_range(start='2024-01-01', periods=12, freq='ME')
 
-# Establish synthetic demand rows
-synthetic_demand = []
+def generate_synthetic_data(df, seed=None):
+    # Set seed if inputted
+    if seed is not None:
+        np.random.seed(seed)
 
-for _, row in inventory_df.iterrows():
-    sku = row['SKU']
-    base_demand = row['Monthly Demand']
+    # Define date range (12 months)
+    months = pd.date_range(start='2024-01-01', periods=12, freq='MS')
 
-    for month in months:
-        # Simulate with 10% noise
-        simulated = base_demand + np.random.normal(0, base_demand * 0.1)
-        synthetic_demand.append({
-            "SKU": sku,
-            "date": month,
-            "demand": max(0, round(simulated))  # No negative demand
-        })
+    # Establish synthetic demand rows
+    synthetic_demand = []
 
-# Create synthetic df
-synthetic_df = pd.DataFrame(synthetic_demand)
+    for _, row in df.iterrows():
+        sku = row['SKU']
+        base_demand = row['Monthly Demand']
 
-print(synthetic_df)
+        for month in months:
+            # Simulate with 10% noise
+            simulated = base_demand + np.random.normal(0, base_demand * 0.1)
+            synthetic_demand.append({
+                "SKU": sku,
+                "date": month,
+                "demand": max(0, round(simulated))  # No negative demand
+            })
+
+    # Create synthetic df
+    synthetic_df = pd.DataFrame(synthetic_demand)
+
+    return synthetic_df
+
+# create csv
+synthetic_df = generate_synthetic_data(inventory_df)
+
+# save as csv
+output_path = "../data/processed/synthetic_monthly.csv"
+
+
+if __name__ == "__main__":
+    if not os.path.exists(output_path):
+        df = generate_synthetic_data()
+        df.to_csv(output_path, index=False)
+        print("Data generated and saved.")
+    else:
+        print("File already exists, skipping generation.")
+
+
